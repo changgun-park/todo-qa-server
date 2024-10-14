e9210e006ed0dc3ef936e8294b0a9f768643d9b5
changgun-park <pcg0537@gmail.com>
2024-10-14 15:51:06 +0900

    feat: todo controller 정의

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Get()
+  getTodos() {
+    return this.todosService.getTodos();
+  }
+
+  @Post()
+  addTodo(@Body('title') title: string) {
+    return this.todosService.addTodo(title);
+  }
+
+  @Put(':id')
+  toggleComplete(@Param('id') id: number) {
+    return this.todosService.toggleComplete(id);
+  }
+
+  @Delete(':id')
+  deleteTodo(@Param('id') id: number) {
+    return this.todosService.deleteTodo(id);
+  }
+}
